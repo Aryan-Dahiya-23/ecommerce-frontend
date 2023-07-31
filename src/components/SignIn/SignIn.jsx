905a24ec59687c8d052149605564207f574206b1
Aryan Dahiya <114468859+Aryan-Dahiya-23@users.noreply.github.com>
2023-07-31 17:31:34 +0530

    Update SignIn.jsx

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -164,7 +164,7 @@ export default function SignIn() {
                         </Grid>
                     </Box>
                 </Box>
-{/*                 <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+                <Copyright sx={{ mt: 8, mb: 4 }} Audioerse />
             </Container>
         </ThemeProvider>
     );
