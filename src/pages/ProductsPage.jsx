c021926361d9f4350f53579734647e67869c9d0e
Aryan Dahiya <114468859+Aryan-Dahiya-23@users.noreply.github.com>
2023-07-31 17:46:06 +0530

    Update ProductsPage.jsx

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -15,9 +15,9 @@ const ProductsPage = () => {
         scrollToTop();
     }, []);
 
-    const fetchProducts = async () => {
+     const fetchProducts = async () => {
         try {
-            const response = await fetch("http://localhost:4000/products");
+            const response = await fetch(`${process.env.REACT_APP_URL}/products`);
             const data = await response.json();
             setProducts(data);
         } catch (error) {
