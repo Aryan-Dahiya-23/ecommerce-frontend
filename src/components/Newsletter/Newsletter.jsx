18f070c73ac6c8cf89524cf9a7aa01101171129b
Aryan Dahiya <114468859+Aryan-Dahiya-23@users.noreply.github.com>
2023-09-30 21:18:12 +0530

    Update Newsletter.jsx

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -42,41 +42,40 @@ const Newsletter = () => {
                     </div> */}
 
                       <div className="icon" style={{ marginLeft: "0px" }}>
-                <a
-                    href="https://www.facebook.com/profile.php?id=100010911464728&mibextid=LQQJ4d"
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                >
-                    <FaFacebookF size={14} />
-                </a>
-            </div>
+                        <a
+                           href="https://www.facebook.com/profile.php?id=100010911464728&mibextid=LQQJ4d"
+                           style={{ textDecoration: 'none', color: 'inherit' }}
+                         >
+                            <FaFacebookF size={14} />
+                        </a>
+                     </div>
 
-            <div className="icon">
-                <a
-                    href="https://x.com/aryan0505?s=21&t=7o3raGmwPwkzIDMg4NOEKQ"
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                >
-                    <FaTwitter size={14} />
-                </a>
-            </div>
+                    <div className="icon">
+                     <a
+                       href="https://x.com/aryan0505?s=21&t=7o3raGmwPwkzIDMg4NOEKQ"
+                       style={{ textDecoration: 'none', color: 'inherit' }}
+                     >
+                        <FaTwitter size={14} />
+                     </a>
+                   </div>
 
-            <div className="icon">
-                <a
-                    href="https://instagram.com/aryan___2305?igshid=OGQ5ZDc2ODk2ZA=="
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                >
-                    <FaInstagram size={14} />
-                </a>
-            </div>
+                   <div className="icon">
+                    <a
+                      href="https://instagram.com/aryan___2305?igshid=OGQ5ZDc2ODk2ZA=="
+                      style={{ textDecoration: 'none', color: 'inherit' }}
+                    >
+                       <FaInstagram size={14} />
+                    </a>
+                  </div>
 
-            <div className="icon" style={{ marginRight: "0px" }}>
-                <a
-                    href="https://www.linkedin.com/in/aryan-dahiya-a72320249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                >
-                    <FaLinkedinIn size={14} />
-                </a>
-            </div>
-
+                  <div className="icon" style={{ marginRight: "0px" }}>
+                    <a
+                      href="https://www.linkedin.com/in/aryan-dahiya-a72320249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+                      style={{ textDecoration: 'none', color: 'inherit' }}
+                    >
+                       <FaLinkedinIn size={14} />
+                    </a>
+                 </div>
 
                 </span>
 
