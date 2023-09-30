eb76c90723a8300f10c2959fc13d1ae9cb6bc78e
Aryan Dahiya <114468859+Aryan-Dahiya-23@users.noreply.github.com>
2023-09-30 21:27:42 +0530

    Update Newsletter.jsx

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -7,6 +7,11 @@ import {
 } from "react-icons/fa";
 
 const Newsletter = () => {
+
+    function redirectToSocialMedia(link) {
+    window.open(link, '_blank', 'noopener noreferrer');
+    }
+    
     return (
         <div className="newsletter">
 
@@ -25,20 +30,20 @@ const Newsletter = () => {
 
                 <span className="social-icons">
 
-                 <div className="icon" style={{marginLeft: "0px"}}>
-                        <FaFacebookF size={14} />
+                    <div className="icon" style={{marginLeft: "0px"}}>
+                        <FaFacebookF size={14} onClick={() => redirectToSocialMedia('https://www.facebook.com/profile.php?id=100010911464728&mibextid=LQQJ4d')} />
                     </div>
 
                     <div className="icon">
-                        <FaTwitter size={14} />
+                        <FaTwitter size={14} onClick={() => redirectToSocialMedia('https://x.com/aryan0505?s=21&t=7o3raGmwPwkzIDMg4NOEKQ')} />
                     </div>
 
                     <div className="icon">
-                        <FaInstagram size={14} />
+                        <FaInstagram size={14} onClick={() => redirectToSocialMedia('https://instagram.com/aryan___2305?igshid=OGQ5ZDc2ODk2ZA==')} />
                     </div>
 
                     <div className="icon" style={{marginRight: "0px"}}>
-                        <FaLinkedinIn size={14} />
+                        <FaLinkedinIn size={14} onClick={() => redirectToSocialMedia('https://www.linkedin.com/in/aryan-dahiya-a72320249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app')} />
                     </div>
 
 {/*                       <div className="icon" style={{ marginLeft: "0px" }}>
